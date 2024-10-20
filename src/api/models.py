4ebe44c11703c4425e3df65ced56d4e1cdb9a391
Valentina Rodriguez Chia <valentinarchia@gmail.com>
2024-10-20 03:41:05 +0000

    Adding models and API clients

--- src/api/models.py
+++ src/api/models.py
@@ -18,29 +18,31 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
     
-class Restaurant(db.Model):
+
+    
+class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    location = db.Column(db.String(80), unique=False, nullable=False)
-    phone_number = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(80), unique=False, nullable=False)
-    guests_capacity = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    identification_number = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<Restaurant {self.email}>'
+        return f'<Client {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "user_name": self.user_name,
-            "name": self.name,
-            "location": self.location,
-            "phone_number": self.phone_number,
             "email": self.email,
-            "guests_capacity": self.guests_capacity,
+            "name": self.name,
+            "last_name": self.last_name,
+            "phone_number" : self.phone_number,
+            "identification_number": self.identification_number,
             "is_active": self.is_active
-            # do not serialize the password, it's a security breach
+          # do not serialize the password, its a security breach
         }
+
+ 
--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Restaurant
+from api.models import db, User, Client
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token
@@ -26,91 +26,94 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/restaurants', methods=['GET'])
-def get_restaurants():
-    all_restaurants = list(Restaurant.query.all()) 
-    results = list(map(lambda restaurant: restaurant.serialize(), all_restaurants)) 
+
+@api.route('/clients', methods=['GET'])
+def get_clients():
+    all_clients = list(Client.query.all()) 
+    results = list(map(lambda client: client.serialize(), all_clients)) 
 
     return jsonify(results), 200
 
-@api.route('/restaurant/<int:restaurant_id>', methods=['GET'])
-def get_restaurant(restaurant_id):
-    restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
+@api.route('/client/<int:client_id>', methods=['GET'])
+def get_client(client_id):
+    client = Client.query.filter_by(id=client_id).first()
     
-    if restaurant is None:
-        return jsonify({"error": "Restaurante no encontrado"}), 404
+    if client is None:
+        return jsonify({"error": "Cliente no se encontro"}), 404
     
-    return jsonify(restaurant.serialize()), 200
+    return jsonify(client.serialize()), 200
 
-@api.route("/singup/restaurant", methods=["POST"])
-def singup():
+@api.route("/singup/client", methods=["POST"])
+def singup_client():
 
     body = request.get_json()
     print(body)
-    restaurant = Restaurant.query.filter_by(user_name=body["user_name"]).first()
-    print(restaurant)
-    if restaurant == None:
-        restaurant = Restaurant(email=body["email"], guests_capacity=body["guests_capacity"],location=body["location"],name=body["name"],phone_number=body["phone_number"],user_name=["user_name"],password=body["password"], is_active=True)
-        db.session.add(restaurant)
+    client = Client.query.filter_by(email=body["email"]).first()
+    print(client)
+    if client == None:
+        client = Client(name=body["name"], last_name=body["Lastname"], identification_number=body["id#"], email=body["email"],phone_number=body["phone_number"],password=body["password"], is_active=True)
+        db.session.add(client)
         db.session.commit()
         response_body = {
-            "msg" : "Restaurante creado"
+            "msg" : "Usurio creado con exito"
          }
         return jsonify(response_body), 200
     else:
-       return jsonify(response_body = {"msg" : "el restaurante ya existe"}), 401
+       return jsonify(response_body = {"msg" : "ya se encuentra un usuario registrado"}), 401
     
-@api.route('/restaurant/<int:restaurant_id>', methods=['PUT'])
-def update_restaurant(restaurant_id):
+@api.route('/client/<int:client>', methods=['PUT'])
+def update_client(client_id):
     body = request.get_json()
 
-    restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
+    client = Client.query.filter_by(id=client_id).first()
 
-    if restaurant is None:
-        return jsonify({"error": "Restaurante no encontrado"}), 404
+    if client is None:
+        return jsonify({"error": "Usuario no encontrado"}), 404
 
-    if "user_name" in body:
-        existing_restaurant = Restaurant.query.filter_by(user_name=body["user_name"]).first()
+    if "email" in body:
+        existing_client = Client.query.filter_by(user_name=body["email"]).first()
 
-        if existing_restaurant and existing_restaurant.id != restaurant_id:
-            return jsonify({"error": "El nombre de usuario ya está en uso"}), 400
+        if existing_client and existing_client.id != client_id:
+            return jsonify({"error": "El correo ya está en uso"}), 400
         else:
-            restaurant.user_name = body["user_name"]
+            client.user_name = body["user_name"]
 
     if "name" in body:
-        restaurant.name = body["name"]
-    if "location" in body:
-        restaurant.location = body["location"]
+        client.name = body["name"]
+    if "last_name" in body:
+        client.location = body["last_name"]
+    if "identification_number" in body:
+        client.phone_number = body["identification_number"]
     if "phone_number" in body:
-        restaurant.phone_number = body["phone_number"]
+        client.email = body["phone_number"]
     if "email" in body:
-        restaurant.email = body["email"]
-    if "guests_capacity" in body:
-        restaurant.guests_capacity = body["guests_capacity"]
+        client.email = body["email"]
     if "password" in body:
-        restaurant.password = body["password"]
+        client.password = body["password"]
 
     db.session.commit()
 
-    return jsonify({"msg": "Restaurante actualizado con éxito", "restaurant": restaurant.serialize()}), 200
+    return jsonify({"msg": "Usuario actualizado!", "client": client.serialize()}), 200
 
 
-@api.route('/restaurant/<int:restaurant_id>', methods=['DELETE'])
-def delete_planeta(restaurant_id):
-    restaurant_to_delete = Restaurant.query.filter_by(id=restaurant_id).all()
+@api.route('/client/<int:client_id>', methods=['DELETE'])
+def delete_client_user(client_id):
+    user_to_delete = Client.query.filter_by(id=client_id).all()
 
-    def deleterestaurant(item):
-        return item.id == restaurant_id
+    def deleteclient(item):
+        return item.id == client_id
     
-    seleccion_de_restaurante = list(filter(deleterestaurant, restaurant_to_delete))
+    seleccionar_usuario = list(filter(deleteclient,user_to_delete))
 
-    if len(seleccion_de_restaurante) > 0:
-        restaurante_a_eliminar = seleccion_de_restaurante[0]
-        db.session.delete(restaurante_a_eliminar)
+    if len(seleccionar_usuario) > 0:
+        user_to_delete = seleccionar_usuario[0]
+        db.session.delete(user_to_delete)
         db.session.commit()
 
-        response_body = {"msg": "Se eliminó correctamente"}
+        response_body = {"msg": "Se eliminó usuario correctamente"}
     else:
-        response_body = {"msg": "No se encontró el restaurante"}
+        response_body = {"msg": "No se encontró usuario"}
 
     return jsonify(response_body), 200
+
+
